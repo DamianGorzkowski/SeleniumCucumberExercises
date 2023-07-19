50667ea0c2f32f578c9010b2c28cc711fc181331
DamianGorzkowski <gorzkowski.damian@gmail.com>
2023-07-19 17:25:25 +0200

    finding every must have field in address form

--- src/test/java/pageobjectpattern/mystore/NewAddressFormPage.java
+++ src/test/java/pageobjectpattern/mystore/NewAddressFormPage.java
@@ -1,5 +1,37 @@
 package pageobjectpattern.mystore;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class NewAddressFormPage {
+    @FindBy(id = "field-alias")
+    private WebElement alias;
+    @FindBy(id = "field-address1")
+    private WebElement address;
+    @FindBy(id = "field-city")
+    private WebElement city;
+    @FindBy(id = "field-postcode")
+    private WebElement zipCode;
+    @FindBy(id = "field-id_country")
+    private  WebElement country;
+    @FindBy(id = "field-phone")
+    private WebElement phone;
+
+    public NewAddressFormPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+
+    public void addressFormFill(UserData userData) {
+        alias.sendKeys(userData.getAlias());
+        address.sendKeys(userData.getAddress());
+        city.sendKeys(userData.getCity());
+        zipCode.sendKeys(userData.getZipCode());
+        country.sendKeys(userData.getCountry());
+        phone.sendKeys(userData.getPhone());
+
+    }
 
 }
