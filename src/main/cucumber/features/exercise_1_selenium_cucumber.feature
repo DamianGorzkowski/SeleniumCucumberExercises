d24e70c8a6e6a56bdfa1b9c5ac67cb0ef1e0304c
DamianGorzkowski <gorzkowski.damian@gmail.com>
2023-07-19 15:52:16 +0200

    Given step added - opening url

--- src/main/cucumber/features/exercise_1_selenium_cucumber.feature
+++ src/main/cucumber/features/exercise_1_selenium_cucumber.feature
@@ -1,6 +1,6 @@
 Feature: Resolving first exercise
   Scenario Outline: Successfully added address to account on website https://mystore-testlab.coderslab.pl/index.php and deleted
-    Given https://mystore-testlab.coderslab.pl/index.php opened in web browser
+    Given https://mystore-testlab.coderslab.pl/index.php opened in Google Chrome
     And Sign in button clicked
     When Login and password entered in log form
     And Tile 'Addresses' clicked (in the bottom menu)
--- src/test/java/cucumber/Exercise1SeleniumCucumber.java
+++ src/test/java/cucumber/Exercise1SeleniumCucumber.java
@@ -1,4 +1,40 @@
 package cucumber;
 
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class Exercise1SeleniumCucumber {
+    private WebDriver driver;
+
+    @Given("^([^ ]+) opened in Google Chrome")
+    public void openInGoogleChrome(String url) {
+        this.driver = new ChromeDriver();
+        this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get(url);
+    }
+
+
+
+
+
+//    And Sign in button clicked
+//    When Login and password entered in log form
+//    And Tile 'Addresses' clicked (in the bottom menu)
+//    And Create new address button clicked
+//    And New address form filled with '<Alias>' '<Address>' '<City>' '<Zip/postal code>' '<Country>' '<Phone>'
+//    And Save button clicked
+//    Then Will check if the data in the added address is correct.
+//    And Will delete the above address by clicking "delete"
+//    And Will check if the address has been deleted
+
+
+
+
+
+
 }
